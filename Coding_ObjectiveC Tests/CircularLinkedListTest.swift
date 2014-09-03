0ec1e61ec8a68f18cf161170b0992d11420ab4c3
Alex Kobylinskyi <sfinx87@gmail.com>
2014-09-03 19:25:21 +0300

    Circular Linked List test cases on Swift

--- Coding_ObjectiveC Tests/CircularLinkedListTest.swift
+++ Coding_ObjectiveC Tests/CircularLinkedListTest.swift
@@ -1,14 +1,14 @@
 //
-//  LinkedListTest.swift
+//  CircularLinkedListTest.swift
 //  Coding_ObjectiveC
 //
-//  Created by Alexander Kobylinskyy on 8/29/14.
+//  Created by Alexander Kobylinskyy on 9/3/14.
 //  Copyright (c) 2014 Alex. All rights reserved.
 //
 
 import XCTest
 
-class LinkedListTest: XCTestCase {
+class CircularLinkedListTest: XCTestCase {
 	
 	var list: LinkedList<Int>! = nil
 
@@ -19,10 +19,10 @@ class LinkedListTest: XCTestCase {
     }
     
     override func tearDown() {
-		list = nil
+        list = nil
         super.tearDown()
     }
-	
+
 	func testListIsEmpty() {
 		XCTAssertNotNil(list, "List should be created")
 		XCTAssertTrue(list.isEmpty() == false, "List should not be empty")
@@ -40,28 +40,21 @@ class LinkedListTest: XCTestCase {
 	func testListInsertion() {
 		XCTAssertNotNil(list.head, "List head should be set")
 		XCTAssertNotNil(list.tail, "List tail should be set")
+		XCTAssertEqual(list.head!.next!, list.head!, "List should be circular")
+		
 		let node: Node<Int> = Node(data: 20)
-		list.insertNode(node)
 		if let headNext = list.head?.next {
 			XCTAssertEqual(headNext, node, "Node should be inserted")
 		}
 		else {
-			XCTAssertFalse(1 == 1, "List node has not be inserted")
+			XCTAssertFalse(1 == 1, "List node has not inserted")
 		}
-		if let listTail = list.tail? {
-			XCTAssertEqual(node, listTail, "List tail should be correct")
-		}
-		else {
-			XCTAssertFalse(1 == 1, "List tail has not inserted")
-		}
+		XCTAssertEqual(node, list.tail!, "List tail should be correct")
+		XCTAssertEqual(list.tail!.next!, list.head!, "List should be circular")
 		list.insertData(30)
-		XCTAssertNotNil(node.next, "Node's next should be set")
-		XCTAssertTrue(node.next?.data == 30, "Node should be inserted")
-		XCTAssertTrue(list.tail?.data == 30, "List tail should be correct")
-		list.insertData(40)
-		XCTAssertTrue(node.next?.next?.data == 40, "Node should be inserted")
-		XCTAssertTrue(list.tail?.data == 40, "List tail should be correct")
-		XCTAssertTrue(list.head?.data == 10, "List head should be correct")
+		XCTAssertTrue(list.tail!.data == 30, "List tail should be correct")
+		XCTAssertTrue(list.head!.data == 10, "List head should be correct")
+		XCTAssertEqual(list.tail!.next!, list.head!, "List should be circular")
 	}
 	
 	func testListRemoval() {
@@ -69,20 +62,21 @@ class LinkedListTest: XCTestCase {
 		list.insertNode(node)
 		list.insertData(30)
 		list.insertData(40)
+		
 		list.removeNode(list.head!)
 		XCTAssertNotNil(list.head, "List head should not be nil")
+		XCTAssertEqual(list.tail!.next!, list.head!, "List should be circular")
 		XCTAssertTrue(list.head!.data == 20, "List head should be reassigned")
 		list.removeNode(list.tail!)
+		XCTAssertNotNil(list.head, "List head should not be nil")
 		XCTAssertNotNil(list.tail, "List tail should not be nil")
 		XCTAssertTrue(list.tail!.data == 30, "List tail should be reassigned")
-		list.insertData(40)
 		list.removeData(30)
 		XCTAssertNil(list.findData(30), "List node should be removed")
-		list.removeNode(node)
 		XCTAssertNotNil(list.head, "List head should not be nil")
 		XCTAssertNotNil(list.tail, "List tail should not be nil")
 		XCTAssertNil(list.findData(20), "List node should be removed")
-		list.removeData(40)
+		list.removeNode(node)
 		XCTAssertNil(list.head, "List head should be equal to nil")
 		XCTAssertNil(list.tail, "List tail should be equal to nil")
 	}
@@ -90,16 +84,12 @@ class LinkedListTest: XCTestCase {
 	func testListSearch() {
 		list.insertData(20)
 		list.insertData(30)
-		list.insertData(40)
+		
 		XCTAssertEqual(list.findData(10)!, list.head!, "Head should be found correctly")
-		XCTAssertEqual(list.findData(40)!, list.tail!, "Tail should be found correctly")
-		if let node = list.findData(20) {
-			XCTAssertTrue(node.data == 20, "List node should be found correctly")
-		}
-		else {
-			XCTAssertFalse(1 == 1, "List node has not found correctly")
-		}
-	
+		XCTAssertTrue(list.findData(20)!.data == 20, "List node should be found correctly")
+		XCTAssertEqual(list.findData(30)!, list.tail!, "Tail should be found correctly")
+		XCTAssertTrue(list.head!.data == 10, "Tail should have correct data")
+		XCTAssertTrue(list.tail!.data == 30, "Tail should have correct data")
 		XCTAssertNil(list.findData(50), "Non-existent data should not be found")
 	}
 
